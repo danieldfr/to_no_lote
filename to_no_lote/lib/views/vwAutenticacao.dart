e8627735e6e0ec5a6059d4b4958ef2bc902b30f1
danieldfr <daniel.dfr@outlook.com>
2022-09-30 09:34:03 -0300

    🚧 update wwAutenticacao.dart
    Ajustes na disposição dos componentes

--- to_no_lote/lib/views/vwAutenticacao.dart
+++ to_no_lote/lib/views/vwAutenticacao.dart
@@ -15,92 +15,90 @@ class _vwAutenticacaoState extends State<vwAutenticacao> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Container(
-          //alignment: Alignment.center,
-          color: Colors.white,
-          child: SingleChildScrollView(
-            padding: EdgeInsets.all(50),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.only(bottom: 20),
-                  child: Image.asset(
-                    "images/logotipo.png",
-                    width: 150,
-                    height: 127,
-                  ),
+      body: Container(
+        alignment: Alignment.center,
+        color: Colors.white,
+        child: SingleChildScrollView(
+          padding: EdgeInsets.all(50),
+          child: Column(
+            //mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(bottom: 20),
+                child: Image.asset(
+                  "images/logotipo.png",
+                  width: 150,
+                  height: 127,
                 ),
-                TextField(
-                  keyboardType: TextInputType.number,
-                  decoration: InputDecoration(
-                      labelText: "CPF"
-                  ),
-                  style: TextStyle(
-                      fontSize: 20
-                  ),
-                  controller: _controllerCPF,
+              ),
+              TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    labelText: "CPF"
                 ),
-                TextField(
-                  keyboardType: TextInputType.number,
-                  decoration: InputDecoration(
-                      labelText: "Senha"
-                  ),
-                  style: TextStyle(
-                      fontSize: 20
-                  ),
-                  controller: _controllerSenha,
+                style: TextStyle(
+                    fontSize: 20
                 ),
-                Padding(
-                  padding: EdgeInsets.only(top: 10),
-                  child: GestureDetector(
-                    onTap: (){},
-                    child: Text(
-                      "Recuperar senha.",
-                      textAlign: TextAlign.right,
-                      style: TextStyle(
-                        fontSize: 12,
-                        color: Colors.black,
-                        decoration: TextDecoration.underline,
-                        fontWeight: FontWeight.normal,
-                      ),
+                controller: _controllerCPF,
+              ),
+              TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    labelText: "Senha"
+                ),
+                style: TextStyle(
+                    fontSize: 20
+                ),
+                controller: _controllerSenha,
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 10),
+                child: GestureDetector(
+                  onTap: (){},
+                  child: Text(
+                    "Recuperar senha.",
+                    textAlign: TextAlign.right,
+                    style: TextStyle(
+                      fontSize: 12,
+                      color: Colors.black,
+                      decoration: TextDecoration.underline,
+                      fontWeight: FontWeight.normal,
                     ),
                   ),
                 ),
-                Padding(
-                  padding: EdgeInsets.only(top: 10),
-                  child: RaisedButton(
-                    color: Colors.green,
-                    textColor: Colors.white,
-                    child: Text(
-                      "Entrar",
-                      style: TextStyle(
-                        fontSize: 20,
-                      ),
-                    ),
-                    onPressed: (){},
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(top: 50),
-                  child: GestureDetector(
-                    onTap: (){},
-                    child: Text(
-                      "Cadastrar usuário.",
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        fontSize: 15,
-                        color: Colors.black,
-                        decoration: TextDecoration.underline,
-                        fontWeight: FontWeight.normal,
-                      ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 10),
+                child: RaisedButton(
+                  color: Colors.green,
+                  textColor: Colors.white,
+                  child: Text(
+                    "Entrar",
+                    style: TextStyle(
+                      fontSize: 20,
                     ),
                   ),
+                  onPressed: (){},
                 ),
-              ],
-            ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 20),
+                child: GestureDetector(
+                  onTap: (){},
+                  child: Text(
+                    "Cadastrar usuário.",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 15,
+                      color: Colors.black,
+                      decoration: TextDecoration.underline,
+                      fontWeight: FontWeight.normal,
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ),
